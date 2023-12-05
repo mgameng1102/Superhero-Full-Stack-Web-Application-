e2ec8f3bf6fb4ae1f563fec3da2bd8ca675748c7
mgameng1102 <mgameng@uwo.ca>
2023-12-05 00:44:23 -0500

    update Schema

--- server/User.js
+++ server/User.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const superheroListSchema = new Schema({
+  listName: { type: String, required: true },
+  description: { type: String },
+  visibility: { type: String, default: 'private' },
+  heroes: { type: Array, default: [] },
+  reviews: { type: Array, default: [] },
+  lastModified: { type: Date, default: Date.now },
+});
+
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true },
@@ -8,8 +17,9 @@ const userSchema = new Schema({
   nickname: { type: String, required: true },
   salt: { type: String, required: true },
   verified: { type: Boolean, default: false },
+  privilege:{ type: Boolean, default: false},
   disabled: { type: Boolean, default: false },
-  superheroLists: { type: Array, default: [] },
+  superheroLists: { type: [superheroListSchema], default: [] },
 });
 
 const User = mongoose.model('User', userSchema);
